05f59ec5a93f4b0217bf5709cc2ab8e44e88f16b
Scott Ruckh <github@gemneye.org>
2025-08-27 23:07:11 -0700

    fix: Update flash-attn wheel and set PYTHONUNBUFFERED

--- Dockerfile
+++ Dockerfile
@@ -2,6 +2,8 @@ FROM runpod/pytorch:2.2.0-py3.10-cuda12.1.1-devel-ubuntu22.04
 
 WORKDIR /
 
+ENV PYTHONUNBUFFERED=1
+
 COPY bootstrap.sh .
 
 CMD ["./bootstrap.sh"]
--- bootstrap.sh
+++ bootstrap.sh
@@ -10,7 +10,7 @@ cd /workspace
 # Install Python dependencies
 pip install --no-cache-dir -r requirements.txt
 pip install git+https://github.com/huggingface/diffusers
-pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.3/flash_attn-2.7.3+cu12torch2.2cxx11abiTRUE-cp310-cp310-linux_x86_64.whl
+pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.3/flash_attn-2.7.3+cu12torch2.2cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
 
 # Execute the application
 python /workspace/app.py
