6aa9c8d167d8f730fe5627117669c398571651f1
Scott Ruckh <github@gemneye.org>
2025-08-28 20:55:58 -0700

    fix: Remove xpu related files from diffusers installation

--- bootstrap.sh
+++ bootstrap.sh
@@ -11,9 +11,8 @@ cd /workspace
 pip install --no-cache-dir -r requirements.txt
 pip install git+https://github.com/huggingface/diffusers
 
-# Patch diffusers to remove xpu reference
-sed -i 's/"xpu": torch.xpu.device_count,/\/\/"xpu": torch.xpu.device_count,/g' /usr/local/lib/python3.10/dist-packages/diffusers/utils/torch_utils.py
-sed -i 's/"xpu": torch.xpu.empty_cache,/\/\/"xpu": torch.xpu.empty_cache,/g' /usr/local/lib/python3.10/dist-packages/diffusers/utils/torch_utils.py
+# Remove xpu related files from diffusers
+find /usr/local/lib/python3.10/dist-packages/diffusers -name "*xpu*" -delete
 
 pip install https://github.com/Dao-AILab/flash-attention/releases/download/v2.7.3/flash_attn-2.7.3+cu12torch2.2cxx11abiFALSE-cp310-cp310-linux_x86_64.whl
 
